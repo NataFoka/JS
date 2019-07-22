67fe7f21502c638a97b6150bf582bfefcd0b7f69
NataFoka <52239879+NataFoka@users.noreply.github.com>
2019-07-22 23:24:38 +0300

    Add files via upload

--- gameFNS.js
+++ gameFNS.js
@@ -5,35 +5,51 @@ document.body.style.padding = "20px 10px 10px 10px";
 
 
 var parentElem = document.body;
-var div = document.createElement('div');
+div = document.createElement('div');
 var div2 = document.createElement('div');
 var div3 = document.createElement('div');
 
 var textElem = document.createTextNode('Тут был я');
-var button=document.createElement("button");
+var button1=document.createElement("button");
+var button2=document.createElement("button");
+
+div.innerHTML = "<strong>Игра змейка.</strong><br>";
 
 
-
-
-
-//div.className = "alert alert-success";
-div.innerHTML = "<strong>Игра змейка.</strong>";
-//div2.innerHTML = "<strong>Ура!</strong> ";
-
-//const text = document.createTextNode( "Mybutton" ); // создаем текстовое содержимое
-//  button.appendChild( text ); // добавляем текстовое содержимое элементу <button>
-//button.appendChild(document.createTextNode("хххззз"));
-
-//<button id="easy" onclick="EasyStart()">Простой</button>
-//button.id
-
-div.style="width: 400px; color: #6B8E23; height:100px; display:inline-block; vertical-align: top; margin: 0px 80px 0px 10px; font-size: 24px; text-align: center";
-div2.style="background: url('209.jpg'); width: 400px; border:2px solid #556B2F;  height:300px; padding: 12px 0px 12px 12px;  display:inline-block; vertical-align: middle";
+div.style="width: 400px;  color: #6B8E23; height:70px; display:inline-block; vertical-align: top; margin: 0px 80px 0px 10px; font-size: 24px; text-align: center; padding: 10px;";
+div2.style="background: url('209.jpg'); width: 400px; border:2px solid #556B2F;  height:300px;   display:inline-block; vertical-align: middle";
 div3.style="background: red; width: 400px; border:1px solid #000000;  height:300px; padding: 12px 0px 12px 12px; display:inline-block; vertical-align: middle";
 
-button.innerHTML = "twxt";
+button1.innerHTML = "Начало игры";
+button1.className = "button";
 
-parentElem.appendChild(div);
+div.appendChild(button1);
+button1.onclick = Start;
+button2.innerHTML = "Конец игры";
+button2.className = "button";
+
+document.body.appendChild(div);
 parentElem.appendChild(div2);
-//parentElem.appendChild(div3);
-//div2.appendChild(button);
+div.appendChild(button2);
+
+
+function Start()
+{
+  alert("Не работает пока!");
+}
+
+GamePlane(20,25);
+
+function GamePlane (SizeX, SizeY){
+  for ( var x = 0; x < SizeX; x++){
+    var Xpole = document.createElement('div');
+      div2.appendChild(Xpole);
+      Xpole.className = 'Stroka';
+      for (var y = 0; y < SizeY; y++){
+        var Ypole = document.createElement('div');
+        Xpole.appendChild(Ypole);
+        Ypole.className = 'cell';
+        Ypole.id = x+','+y;
+      }
+    }
+}
